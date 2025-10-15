9698a3895d6a0855f10a1a3a6645317446e4821a
Marcos <marcosandrecorreiacosta@gmail.com>
2025-10-15 04:26:55 +0100

    utility func (get_id update_id log), progress on get_metadata and delete_file

--- recycle_bin.sh
+++ recycle_bin.sh
@@ -26,11 +26,110 @@ initialize_recyclebin(){
     
     #A flag -p faz com que mkdir não dê erro caso o dir ja exista, cria qualquer parent dir nessesario, e não apaga conteudo caso ja existam
     mkdir -p "$RECYCLE_BIN_DIR/files"
+    #verificar se os ficheiros que precisam de ser inicializados com dados pre-existentes ja existem para evitar sobescrever dados no caso de dupla chamada da função
     if [ ! -f $METADATA_FILE ]; then
         printf "ID,ORIGINAL_NAME,ORIGINAL_PATH,DELETION_DATE,FILE_SIZE,FILE_TYPE,PERMISSIONS,OWNER\n" > $METADATA_FILE    
+    fi
+
+    if [ ! -f "$RECYCLE_BIN_DIR/config" ]; then
+        printf "MAX_SIZE_MB=1024\n0\n" > "$RECYCLE_BIN_DIR/config"  
     fi 
-    echo "MAX_SIZE_MB=1024" > "$RECYCLE_BIN_DIR/config"
     touch "$RECYCLE_BIN_DIR/recyclebin.log"
     return 0
 }
 
+#################################################
+# Function: log
+# Description: Log the parameter with a costum timestamp
+# Parameters: 1
+# Returns: 0 on success
+#################################################
+log(){
+    local log="$1"
+    if [ $# -ne 1 ]; then
+        echo "Erro: log takes 1 argument only"
+        echo "$(date +"[%d/%m/%Y %H:%M:%S]"): ERROR: log takes 1 argument only" >> $RECYCLE_BIN_DIR/recyclebin.log
+        return 1
+    fi
+
+    echo "$(date +"[%d/%m/%Y %H:%M:%S]"): $log" >> $RECYCLE_BIN_DIR/recyclebin.log
+    return 0
+}
+#################################################
+# Function: get_id
+# Description: Ler a linha 2 do ficheiro config que contem the last issued ID
+# Parameters: None
+# Returns: 0 on success
+#################################################
+get_id(){
+    echo $(head -n 2 "$RECYCLE_BIN_DIR/config" | tail -n 1)
+    return 0
+}
+#################################################
+# Function: update_id
+# Description: Sobe o numero do ultimo Id por q
+# Parameters: None
+# Returns: 0 on success
+#################################################
+update_id(){
+    local new_id_counter=$(($(get_id) + 1))
+    sed -i "2c $new_id_counter" $RECYCLE_BIN_DIR/config
+    return 0
+}
+
+#################################################
+# Function: get_metadata
+# Description: Obter a metadata de um ficheiro
+# Parameters: Apenas 1
+# Returns: 0 on success
+#################################################
+get_metadata(){
+    if [ $# -ne 1 ]; then
+        echo "Erro: get_metadata takes 1 argument only"
+        echo "$(date +"[%d/%m/%Y %H:%M:%S]"): ERROR: get_metadata takes 1 argument only" >> $RECYCLE_BIN_DIR/recyclebin.log
+        return 1
+    fi
+    local file="$1"
+
+    #Values we need to get:
+    local og_filename="$file"
+    local og_abspath="$(realpath "$file")"
+    local del_time="$(date +"[%Y/%m/%d %H:%M:%S]")"
+    local file_size=&(( -f "$file" ? $(stat -c %s "$file") : $(du -sb "$file") ))
+
+    return 0
+}
+
+#################################################
+# Function: delete_file
+# Description: Apagar ficheiros
+# Parameters: Pelo menos 1
+# Returns: 0 on success
+#################################################
+delete_file(){
+    #Para cada um dos argumentos fazer o seguinte
+    id_counter=$(get_id)
+    for var in "$@"
+    do 
+        #Skip invalid input
+        if [ ! -f $var ] && [ ! -d $var ]; then 
+            echo "\"$var\" isn't a filename or directory"
+            log "Failed to delete $(pwd)/$var - There is no such File/Dir"
+            continue
+        else 
+            #Caso input seja valido gerar novo Id
+             ##Alterar o documento com o novo Id
+            new_id_counter=$(($id_counter + 1))
+            sed -i "2c $new_id_counter" $RECYCLE_BIN_DIR/config
+             ##Atualizar a variavel Id_Counter para o novo Id
+            id_counter=$(get_id)
+        fi
+
+
+        mv "$var" "$RECYCLE_BIN_DIR/files/$id_counter"
+
+        echo "$(realpath "$var") was deleted"
+        log "$(realpath "$var") Was deleted; ID:$id_counter"
+
+    done
+}
